66c8dc3d985f1258dc8ca88199c0f708e30ef096
BayajidAlam <bayajidalam2001@gmail.com>
2023-08-03 01:07:38 +0600

    product details page done

--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -4,11 +4,27 @@ import Image from "next/image";
 import React from "react";
 
 const ProductDetailsPage = ({ pcData }) => {
-  console.log(pcData);
+  // console.log(pcData.key_features);
+
+  function getAllKeyValuePairs(obj) {
+    const keyValuePairs = [];
+
+    for (const key in obj) {
+      if (Object.hasOwnProperty.call(obj, key)) {
+        keyValuePairs.push({ key: key, value: obj[key] });
+      }
+    }
+
+    return keyValuePairs;
+  }
+  const pcFeaturesInFeatures = getAllKeyValuePairs(pcData?.key_features);
+
+  const ratings = getAllKeyValuePairs(pcData?.individual_rating);
+
   return (
     <>
       <section class="text-gray-600 body-font overflow-hidden">
-        <div class="container px-5 py-24 mx-auto">
+        <div class="container px-5 lg:pt-24 pt-4 pb-16 mx-auto">
           <div class="lg:w-4/5 mx-auto flex flex-wrap">
             <Image
               class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
@@ -82,48 +98,76 @@ const ProductDetailsPage = ({ pcData }) => {
                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                   </svg>
                   <span class="text-gray-600 ml-3">
-                    {pcData?.average_rating} average Reviews
+                    {pcData?.average_rating} <span className="text-lg">average Reviews</span>
                   </span>
                 </span>
-                <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                  <a class="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a class="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a class="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
+        
               </div>
+              {ratings.map((rating, i) => (
+                <div key={i} class="flex mb-4">
+                  <span class="flex items-center">
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-4 h-4 text-indigo-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-4 h-4 text-indigo-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-4 h-4 text-indigo-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-4 h-4 text-indigo-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-4 h-4 text-indigo-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <span class="text-gray-600 ml-3">
+                      {rating.key} {rating.value}
+                    </span>
+                  </span>
+                  
+                </div>
+              ))}
               <p class="leading-relaxed">{pcData?.description}</p>
               <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                 <div class="flex">
@@ -161,45 +205,47 @@ const ProductDetailsPage = ({ pcData }) => {
           <div class="lg:w-4/5 mx-auto flex flex-wrap">
             <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
               <div class="flex mb-4">
-                <a class="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
+                <a class="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-2xl font-bold px-1">
                   Description
                 </a>
               </div>
-              <p class="leading-relaxed mb-4">
-                {pcData?.description}
-              </p>
-              <div class="flex border-t border-gray-200 py-2">
-                <span class="text-gray-500">Color</span>
-                <span class="ml-auto text-gray-900">Blue</span>
-              </div>
-              <div class="flex border-t border-gray-200 py-2">
-                <span class="text-gray-500">Size</span>
-                <span class="ml-auto text-gray-900">Medium</span>
-              </div>
-              <div class="flex border-t border-b mb-6 border-gray-200 py-2">
-                <span class="text-gray-500">Quantity</span>
-                <span class="ml-auto text-gray-900">4</span>
-              </div>
-              <div class="flex">
-                <span class="title-font font-medium text-2xl text-gray-900">
-                  $58.00
-                </span>
-                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                  Button
-                </button>
-                <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
-                </button>
-              </div>
+              <p class="leading-relaxed mb-4">{pcData?.description}</p>
+
+              {pcFeaturesInFeatures.map((feature, i) => (
+                <div key={i} class="flex border-t border-gray-200 py-2">
+                  <span class="text-gray-500">{feature?.key}</span>
+                  <span class="ml-auto text-gray-900">{feature?.value}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
+      <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container  mx-auto">
+          <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              {
+                pcData.reviews.map((review,i) => <div 
+                key={i}
+                class="lg:w-2/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <Image 
+                  className="w-12 h-12 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  width={500}
+                  height={
+                    500
+                  }alt="okk"
+                  src="https://dummyimage.com/84x84"></Image>
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">{review.username}</h2>
+                    <p class="text-gray-500">{review.comment}</p>
+                  </div>
+                </div>
+              </div>)
+              }
             </div>
           </div>
         </div>
